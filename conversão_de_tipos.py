0305ffcbc65761c37996f4ef065ae13528420981
Adeilson de Lima Nascimento <143619894+AdeilsondeLimaNa@users.noreply.github.com>
2024-10-05 12:36:55 -0300

    Add files via upload

---
+++ conversão_de_tipos.py
@@ -0,0 +1,30 @@
+preco = 10
+print(preco)
+
+preco = float(preco)
+print(preco)
+
+preco = 10 / 2
+print(preco)
+
+preco = 10 // 2
+print(preco)
+
+preco = 12.0
+print(preco)
+
+preco = int(preco)
+print(preco)
+
+print(int(1.98044))
+print(int("10"))
+print(float("10.10"))
+print(float(100))
+
+valor = 10
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+print(100 / 2)
+print (100 // 2)
---
+++ tipos_primitivos.py
@@ -0,0 +1,12 @@
+nome = "Adeilson"
+idade = 29
+peso = 68.9
+verdadeiro = (True)
+falso = (False)
+
+print(f"Meu nome é {nome} e tenho {idade} anos, peso {peso} kg")
+
+str
+int
+float
+bool
